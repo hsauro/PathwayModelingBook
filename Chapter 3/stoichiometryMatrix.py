c2e5780f13915b71c739ccce740a93eae7fe4dec
Herbert Sauro <hsauro@u.washington.edu>
2019-09-11 17:24:09 -0700

    Update and rename test.txt to stoichiometryMatrix.py

---
+++ Chapter 3/stoichiometryMatrix.py
@@ -0,0 +1,12 @@
+import tellurium as te             
+r = te.loada ('''                  
+   J1: A -> B; k1*A - k2*B;        
+   J2: B -> C; k3*B - k4*C;        
+   k1 = 0.1; k2 = 0.02;            
+   k3 = 0.3; k4 = 0.04;            
+   A  = 10;  B  = 0; C = 0;        
+''')                               
+print (r.getFullStoichiometryMatrix(})
+print (r.getReactionIds())       
+print (r.getFloatingSpeciesIds())
+
