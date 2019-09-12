153d1c69377c757702345dcdba1f0078c68b98c1
Herbert Sauro <hsauro@u.washington.edu>
2019-09-11 17:44:23 -0700

    Create stableSpiral.py

---
+++ Chapter 12/stableSpiral.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import tellurium as te
+
+Y, X = np.mgrid[-4:4:100j, -4:4:100j]
+
+r = te.loada('''
+    x' = -0.5*x - y;
+    y' =  x - y;
+    
+    x = 4; y = -4;
+''')
+
+m = r.simulate (0, 8, 100)
+
+U =  -0.5*X - Y
+V =  X - Y
+
+plt.subplots(1,2, figsize=(10,4))
+plt.subplot(121)
+plt.xlabel('x', fontsize='16')
+plt.ylabel('y', fontsize='16')
+plt.streamplot(X, Y, U, V, density=[1, 1])
+
+plt.ylim((-4,4))
+plt.xlim((-4,4))
+
+plt.axhline(0, color='black')
+plt.axvline(0, color='black')
+
+plt.subplot(122)
+
+plt.ylim((-5,5))
+plt.xlim((0,8))
+plt.xlabel('Time', fontsize='13')
+
+
+plt.plot (m[:,0], m[:,1], color='r', linewidth=2, label='x')
+plt.plot (m[:,0], m[:,2], color='b', linewidth=2, label='y')
+plt.legend()
+
+plt.savefig ('c:\\tmp\\phase.pdf')
+
+plt.show()
