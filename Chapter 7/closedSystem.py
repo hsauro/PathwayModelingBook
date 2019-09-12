25f7a54ed8eaaf52b96aeaad1d4465cfba45462c
Herbert Sauro <hsauro@u.washington.edu>
2019-09-11 17:28:34 -0700

    Create closedSystem.py

---
+++ Chapter 7/closedSystem.py
@@ -0,0 +1,14 @@
+# Simulation of a simple closed system
+import tellurium as te
+
+# Simulation of a simple closed system
+r = te.loada ('''
+    A -> B; k1 * A;
+    B -> A; k2 * B;
+
+    A = 10; B = 0;
+    k1 = 1; k2 = 0.5;
+''')
+
+result = r.simulate(0, 6, 100)
+r.plot()
